a7e1878484fd7b0220660ad17c6fd2bb28447cd1
klequis <klequis@gmail.com>
2019-03-20 19:17:36 -0700

    with error from api

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,3 +1,66 @@
+
+// eslint-disable-next-line
+import { orange, red } from '../../logger'
+
+export const logError = (err, key) => {
+
+  red(`actions.logError(key:${key})`, err)
+}
+export const logReturnValue = (value) => {
+  return ({
+    type: 'app/noAction'
+  })
+}
+
+const markRequestPending = (key) => {
+  return {
+    type: 'app/markRequestPending',
+    meta: { key },
+  }
+}
+
+const markRequestSuccess = (key) => {
+  return ({
+    type: 'app/markRequestSuccess',
+    meta: { key },
+  })
+}
+
+const markRequestFailed = (reason, key) => {
+  return {
+    type: 'app/markRequestFailed',
+    payload: reason,
+    meta: { key },
+  }
+}
+
+export const createRequestThunk = ({ request, key, start = [], success = [], failure = [] }) => {
+  red('createRequestThunk: key', key)
+  red('createRequestThunk: request', request)
+  return (...args) => (dispatch) => {
+    const requestKey = (typeof key === 'function') ? key(...args) : key
+    start.forEach((actionCreator) => {
+      dispatch(actionCreator())
+    })
+    dispatch(markRequestPending(requestKey))
+    return request(...args)
+      .then((data) => {
+        success.forEach((actionCreator) => {
+          dispatch(actionCreator(data))
+        })
+        dispatch(markRequestSuccess(requestKey))
+      })
+      .catch((reason) => {
+        failure.forEach((actionCreator) => dispatch(actionCreator(reason)))
+        dispatch(markRequestFailed(reason, requestKey))
+      })
+  }
+}
+
+
+
+//////////////////////////////////////////////////
+
 let nextTodoId = 0
 
 export const ADD_TODO = "ADD_TODO"
@@ -20,6 +83,16 @@ export const setFilter = filter => ({ type: SET_FILTER, payload: { filter } })
 
 ///////////////////////////////
 
+export const TODO_READ_KEY = 'TODO_READ_KEY'
+const todoRead = (todos) => {
+  console.log('todos', todos);
+
+  return ({
+    type: TODO_READ_KEY,
+    payload: {todos}
+  })
+}
+
 const rejectErrors = (res) => {
   const { status } = res
 
@@ -33,19 +106,6 @@ const rejectErrors = (res) => {
     error: res.json()
   })
 }
-
-export const TODO_READ_KEY = 'TODO_READ_KEY'
-export const todoRead = (todos) => {
-  console.log('todos', todos);
-
-  return ({
-    type: TODO_READ_KEY,
-    payload: {todos}
-  })
-}
-
-
-
 
 export const fetchJson = (url, options = {}) => {
   let headers = {
@@ -63,6 +123,29 @@ export const fetchJson = (url, options = {}) => {
   )
 }
 
+// api
+const todos = {
+  async read(user) {
+    try {
+      const data = await fetchJson(
+        '/api/todo',
+        {
+          method: 'GET',
+        }
+      )
+      return data.data
+    }
+    catch (e) {
+      const error = await e.error
+      throw error
+    }
+  },
+}
+
+
+
+
+
 // using fetchJson
 // export const getTodos = () => {
 //   const headers = {
@@ -78,23 +161,53 @@ export const fetchJson = (url, options = {}) => {
 
 // }
 
+// export const createRequestThunk = ({ request, nextAction }) => {
+//   return (...args) => (dispatch) => request(...args)
+//   .then((data) => {
+//     dispatch(nextAction(data))
+//   })
+//   .catch((reason) => {
+//     console.log('ERROR')
+//   })
+// }
+
+// const todoRequest = async () => {
+//   try {
+//     const data = await fetchJson(
+//       '/api/todo',
+//       {
+//         method: 'GET',
+//       }
+//     )
+//     const o = data.data
+//     console.log('o', o)
+//     return o
+//   }
+//   catch (e) {
+//     const error = await e.error
+//     throw error
+//   }
+// }
+
+export const getTodos = createRequestThunk({
+  request: todos.read,
+  key: 'someKey',
+  success: [ todoRead ]
+})
+
+
+
 // not using fetchJson
-export const getTodos = () => {
-  const headers = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-  }
-  return (dispatch) => fetch('/api/todo', {'headers': headers})
-    .then((res) => res.json())
-    // .then((data) => console.log('data', data.data))
-    .then((data) => dispatch(todoRead(data)))
-    .catch((reason) => {
-      console.log('getTodos failed', reason)
-
-    })
-
-}
-
-// dispatch(todoRead(data))
-
-// todos => dispatch(todoRead(todos))
+// export const getTodos2 = () => {
+//   const headers = {
+//     'Accept': 'application/json',
+//     'Content-Type': 'application/json',
+//   }
+//   return (dispatch) => fetch('/api/todo', {'headers': headers})
+//     .then((res) => res.json())
+//     // .then((data) => console.log('data', data.data))
+//     .then((data) => dispatch(todoRead(data)))
+//     .catch((reason) => {
+//       console.log('getTodos failed', reason)
+//     })
+// }
