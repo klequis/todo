ba9eab83f164587823817a90ca0b8dd034e11545
klequis <klequis@gmail.com>
2026-06-25 14:18:37 -0700

    change preset: from "vercel" to "process.env.NITRO_PRESET"

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { solidStart } from "@solidjs/start/config";
 import { qrcode } from "vite-plugin-qrcode";
 import mkcert from "vite-plugin-mkcert";
 
-const nitroConfig = process.env.VERCEL ? { preset: "vercel" } : {};
+const nitroConfig = process.env.NITRO_PRESET ? { preset: process.env.NITRO_PRESET } : {};
 
 export default defineConfig({
   plugins: [solidStart(), nitro(nitroConfig), qrcode(), mkcert()],
