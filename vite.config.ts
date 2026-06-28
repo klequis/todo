284a0eaf5e7aa9515050a19d3bc37dc2bd552411
klequis <klequis@gmail.com>
2026-06-28 09:49:04 -0700

    removed mksert plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from "vite";
 import { nitroV2Plugin as nitro } from "@solidjs/vite-plugin-nitro-2";
 import { solidStart } from "@solidjs/start/config";
 import { qrcode } from "vite-plugin-qrcode";
-import mkcert from "vite-plugin-mkcert";
 
 const nitroConfig = process.env.NITRO_PRESET ? { preset: process.env.NITRO_PRESET } : {};
 
 export default defineConfig({
-  plugins: [solidStart(), nitro(nitroConfig), qrcode(), mkcert()],
+  plugins: [solidStart(), nitro(nitroConfig), qrcode()],
 });
